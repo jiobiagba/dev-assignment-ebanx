25f8125fe552278e093d7b333e0f7f3b324e05bd
jiobiagba <j.i.obiagba2016@gmail.com>
2020-08-15 01:06:33 +0100

    test for reset endpoint added

--- test/account.test.js
+++ test/account.test.js
@@ -13,6 +13,17 @@ describe("Basic Checks and Resetting", function() {
                 done()
             })
     })
+
+    it("resets state before starting other tests", function(done) {
+        request(app)
+            .post("/reset")
+            .expect(200)
+            .end((err, res) => {
+                if(err) console.error("\nError in testing reset:    ", err)
+                expect(res.text).toBe("OK")
+                done()
+            })
+    })
 })
 
 setTimeout(() => process.exit(0), 5000)
